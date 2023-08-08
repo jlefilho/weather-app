d131ea357cb8553d55c517079b1958e53234d94b
Jorge Espindola Filho <jlespindolaf@gmail.com>
2023-08-08 20:05:38 -0300

    feat: created weather service

---
+++ src/app/environments/weather.ts
@@ -0,0 +1,1 @@
+export const API_key: string = '94dcee9e6fa90b1e2b01eeddaa32856d'
---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_key } from 'src/app/environments/weather';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey = API_key
+  private apiURL = 'api.openweathermap.org/data/2.5/weather'
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string): Observable<any> {
+    return this.http.get(`${this.apiURL}?q=${cityName}&units=metric&mode=json&APPID=${this.apiKey}`, {})
+  }
+}
