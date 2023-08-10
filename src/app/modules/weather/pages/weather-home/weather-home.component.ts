4804a4dd44c76821490d70122d4b1f0625aaca87
Jorge Espindola Filho <jlespindolaf@gmail.com>
2023-08-10 12:20:43 -0300

    chore: observable unsubscription

--- src/app/modules/weather/pages/weather-home/weather-home.component.ts
+++ src/app/modules/weather/pages/weather-home/weather-home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherData } from 'src/app/models/interfaces/weatherData';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
   private defaultCityName = 'Porto Alegre'
   private weatherData!: WeatherData
+  private readonly destroy$: Subject<void> = new Subject()
 
   constructor(private weatherService: WeatherService) { }
 
@@ -17,8 +19,16 @@ export class WeatherHomeComponent implements OnInit {
     this.getWeatherData(this.defaultCityName)
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next()
+    this.destroy$.complete()
+  }
+
   getWeatherData(cityName: string):void{
     this.weatherService.getWeatherData(cityName)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
       .subscribe({
         next: (response) => {
           response && (this.weatherData = response)
